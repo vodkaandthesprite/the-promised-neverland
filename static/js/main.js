4ee9311fc121b01a6778e039c08dac1545163599
melmoriii <polyna.myroshnychenko@gmail.com>
2021-09-04 15:26:44 +0300

    grouped test.js and main.js codes; removed old js strings

--- static/js/main.js
+++ static/js/main.js
@@ -1,31 +1,18 @@
-const auto = document.getElementById("author");
-var entrance = auto.style;
+const author = document.getElementById("author");
+var entrance = author;
 console.log(entrance);
 function enter(){
-    if (auto.style.display === 'block') {
-        auto.style.display = 'none';
+    if (author.style.display === 'block') {
+        author.style.display = 'none';
     } else {
-        auto.style.display = 'block';
+        author.style.display = 'block';
     }
 }
-const el1 = document.getElementsByClassName("season");
-var season1 = el1[0].style;
-console.log(season1);
-function s1(){
-    season1.backgroundColor = ("#cbc5c1");
-    season1.color = ("#524d42");
-}
-const el2 = document.getElementsByClassName("season");
-var season2 = el2[1].style;
-console.log(season2);
-function s2(){
-    season2.backgroundColor = ("#cbc5c1");
-    season2.color = ("#524d42");
-}
-const el3 = document.getElementsByClassName("season");
-var season3 = el3[2].style;
-console.log(season3);
-function s3(){
-    season3.backgroundColor = ("#cbc5c1");
-    season3.color = ("#524d42");
+let seasons = document.getElementsByClassName("season");
+for (let i = 0;seasons.length;i++) {
+    seasons[i].addEventListener("click", reColor);
+    function reColor() {
+        seasons[i].style.backgroundColor = ("#cbc5c1");
+        seasons[i].style.color = ("#524d42");
+    }
 }
--- static/js/test.js
+++ static/js/test.js
@@ -1,21 +1,9 @@
-// let password = '';
-// let rightPassword = 'qwerty';
-// while (password != rightPassword){
-// 	password = prompt('Введите пароль:', '');
-// 	if (password == null){
-// 		break;
-// 	}
-// }
-let seasons = document.getElementsByClassName("season");
+var seasons = document.getElementsByClassName("season");
 // seasons.forEach(makeActive);
 for (let i = 0;seasons.length;i++) {
-    seasons[i].style.backgroundColor = ("#cbc5c1");
-    seasons[i].style.color = ("#524d42");
+    seasons[i].addEventListener("click", reColor);
+    function reColor() {
+        seasons[i].style.backgroundColor = ("#cbc5c1");
+        seasons[i].style.color = ("#524d42");
+    }
 }
-// function makeActive(season) {
-//     season.addEventListener("click", reColor);
-//     function reColor() {
-//         season.style.backgroundColor = ("#cbc5c1");
-//         season.style.color = ("#524d42");
-//     }
-// }
