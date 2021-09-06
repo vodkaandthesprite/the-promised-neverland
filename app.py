143d1b5d4728c38e7093174378a55d12a5ec3b94
melmoriii <polyna.myroshnychenko@gmail.com>
2021-09-06 21:22:47 +0300

    active episode btns

--- app.py
+++ app.py
@@ -3,17 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 name = "line"
 
+
 @app.route('/')
 def rt():
     return render_template("index.html")
+
 
 @app.route('/watchpage')
 def watch():
     return render_template("watch.html")
 
+
 @app.route('/mangapage')
 def manga():
     return render_template("manga.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -10,9 +10,17 @@ function enter(){
 }
 let seasons = document.getElementsByClassName("season");
 for (let i = 0;seasons.length;i++) {
-    seasons[i].addEventListener("click", reColor);
-    function reColor() {
+    seasons[i].addEventListener("click", s_reColor);
+    function s_reColor() {
         seasons[i].style.backgroundColor = ("#cbc5c1");
         seasons[i].style.color = ("#524d42");
+    }
+}
+let episodes = document.getElementsByClassName("episode");
+for (let i = 0;episodes.length;i++) {
+    episodes[i].addEventListener("click", ep_reColor);
+    function ep_reColor() {
+        episodes[i].style.backgroundColor = ("#cbc5c1");
+        episodes[i].style.color = ("#524d42");
     }
 }
