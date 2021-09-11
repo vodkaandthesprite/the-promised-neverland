16700635c2aaf4f4668b65f22070bafdc8980b0a
melmoriii <polyna.myroshnychenko@gmail.com>
2021-09-11 14:31:55 +0300

    created a function which colors permanently

--- static/js/test.js
+++ static/js/test.js
@@ -1,4 +1,16 @@
-function print(value) {
-    console.log(value);
-    return 0;
+// function print(value) {
+//     console.log(value);
+//     return 0;
+// }
+
+let seasons = document.querySelectorAll(".season");
+let episodes = document.querySelectorAll(".episode");
+const actives = document.querySelectorAll(".season, .episode");
+actives.forEach(trigger);
+function trigger(active) {
+    active.addEventListener("click", reColor);
+    function reColor() {
+        active.style.backgroundColor = "#cbc5c1";
+        active.style.color = "#524d42";
+    }
 }
