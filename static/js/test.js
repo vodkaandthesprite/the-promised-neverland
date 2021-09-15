5a3fa6a14aed516610e5c3ca30a14e97045960c8
melmoriii <polyna.myroshnychenko@gmail.com>
2021-09-15 19:17:28 +0300

    created a function which activates seasons*

--- static/js/test.js
+++ static/js/test.js
@@ -6,16 +6,16 @@
 let seasons = document.querySelectorAll(".season");
 let episodes = document.querySelectorAll(".episode");
 const actives = document.querySelectorAll(".season, .episode");
-actives.forEach(trigger);
+seasons.forEach(trigger);
 function trigger(active) {
     active.addEventListener("click", reColor);
     function reColor() {
+        seasons.forEach(makePassive);
+        function makePassive(season) {
+            season.style.backgroundColor = "#524d42";
+            season.style.color = "#cbc5c1";
+        }
         active.style.backgroundColor = "#cbc5c1";
         active.style.color = "#524d42";
-        active.addEventListener("click", unReColor);
-        function unReColor() {
-            active.style.backgroundColor = "#524d42";
-            active.style.color = "#cbc5c1";
-        }
     }
 }
