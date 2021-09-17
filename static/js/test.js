31895a87a6c7803101afab6dfe5d49b84383ac44
melmoriii <polyna.myroshnychenko@gmail.com>
2021-09-17 13:35:12 +0300

    created a function which activates episodes & seasons

--- static/js/test.js
+++ static/js/test.js
@@ -6,18 +6,17 @@
 function getAll(selector) {
     return document.querySelectorAll(selector);
 }
-// let seasons = getAll(".season");
-// let episodes = getAll(".episode");
-// const actives = getAll(".season, .episode");
 getAll(".season").forEach(trigger);
+getAll(".episode").forEach(trigger);
 function trigger(active) {
     active.addEventListener("click", reColor);
     function reColor() {
-        console.log(active.srcObject);
+        // console.log(active.srcObject);
         getAll(".season").forEach(makePassive);
-        function makePassive(season) {
-            season.style.backgroundColor = "#524d42";
-            season.style.color = "#cbc5c1";
+        getAll(".episode").forEach(makePassive);
+        function makePassive(elements) {
+            elements.style.backgroundColor = "#524d42";
+            elements.style.color = "#cbc5c1";
         }
         active.style.backgroundColor = "#cbc5c1";
         active.style.color = "#524d42";
