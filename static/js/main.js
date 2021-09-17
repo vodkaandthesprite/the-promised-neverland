bacde9b86c46dbfdd93b63d65abbe0eff30e57e2
melmoriii <polyna.myroshnychenko@gmail.com>
2021-09-18 00:00:42 +0300

    moved "watch page btns" code part from test.js to main.js; tried to edit authorisation part of code

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,32 @@
-// const author = document.getElementById("author");
-// var entrance = author;
-// console.log(entrance);
-// function enter() {
-//     if (author.style.display === 'block') {
-//         author.style.display = 'none';
-//     } else {
-//         author.style.display = 'block';
-//     }
-// }
+const entrance = document.getElementById("author"); //header
+console.log(enter());
+function enter() {
+    if (entrance.style.display === 'block') {
+        entrance.style.display = 'none';
+    } else {
+        entrance.style.display = 'block';
+    }
+}
+//watch page btns
+function getAll(selector) {
+    return document.querySelectorAll(selector);
+}
+getAll(".season").forEach(trigger);
+getAll(".episode").forEach(trigger);
+function trigger(active) {
+    active.addEventListener("click", reColor);
+    function reColor() {
+        // console.log(active.srcObject);
+        getAll(".season").forEach(makePassive);
+        getAll(".episode").forEach(makePassive);
+        function makePassive(elements) {
+            elements.style.backgroundColor = "#524d42";
+            elements.style.color = "#cbc5c1";
+        }
+        active.style.backgroundColor = "#cbc5c1";
+        active.style.color = "#524d42";
+    }
+}
 // let seasons = document.getElementsByClassName("season");
 // for (let i = 0;seasons.length;i++) {
 //     seasons[i].addEventListener("click", sReColor);
--- static/js/test.js
+++ static/js/test.js
@@ -2,23 +2,3 @@
 //     console.log(value);
 //     return 0;
 // }
-
-function getAll(selector) {
-    return document.querySelectorAll(selector);
-}
-getAll(".season").forEach(trigger);
-getAll(".episode").forEach(trigger);
-function trigger(active) {
-    active.addEventListener("click", reColor);
-    function reColor() {
-        // console.log(active.srcObject);
-        getAll(".season").forEach(makePassive);
-        getAll(".episode").forEach(makePassive);
-        function makePassive(elements) {
-            elements.style.backgroundColor = "#524d42";
-            elements.style.color = "#cbc5c1";
-        }
-        active.style.backgroundColor = "#cbc5c1";
-        active.style.color = "#524d42";
-    }
-}
