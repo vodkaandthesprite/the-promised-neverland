1db9ee71d8e5c3475ab7dd63bcea312e11ed4edf
melmoriii <polyna.myroshnychenko@gmail.com>
2021-09-24 23:13:35 +0300

    created lib.py dictionary to short the html syntax

---
+++ lib.py
@@ -0,0 +1,19 @@
+class MenuItem:
+    pass
+
+
+main = MenuItem()
+main.name = "Главная"
+watch = MenuItem()
+watch.name = "Смотреть"
+manga = MenuItem()
+manga.name = "Манга"
+authorisation = MenuItem()
+
+
+class Footer():
+    pass
+
+
+footer = Footer()
+footer.name = "Powered by Mel. All rights reserved. ©"
