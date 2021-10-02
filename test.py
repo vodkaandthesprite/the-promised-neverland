6493bca89e46d5fa04d77fdab74de75964774c28
melmoriii <polyna.myroshnychenko@gmail.com>
2021-10-02 22:47:21 +0300

    testing creating, opening & editing files w/python only*

--- test.py
+++ test.py
@@ -25,23 +25,51 @@
 # phrase = f"my name is {person['name']}"
 # phrase1 = f"i have {person['mana']} mana points"
 # phrase2 = f"{person2['name']} is powerful"
-class Animal:
-    def victim(self):
-        print('i\'m an animal \nand not a plant')
-
-    def is_mushroom(self):
-        print("no!")
-        print("of course i'm not a mushroom")
-
-    def voice(self):
-        if self is cat:
-            print("meow")
-        elif self is dog:
-            print("woof")
-        else:
-            print("*silence*")
-
-cat = Animal()
-cat.victim(), cat.voice()
-dog = Animal()
-dog.victim(), dog.voice()
+# print(phrase2)
+# class Animal:
+#     def victim(self):
+#         print('i\'m an animal \nand not a plant')
+#
+#     def is_mushroom(self):
+#         print("no!")
+#         print("of course i'm not a mushroom")
+#
+#     def voice(self):
+#         if self is cat:
+#             print("meow")
+#         elif self is dog:
+#             print("woof")
+#         else:
+#             print("*silence*")
+#
+# cat = Animal()
+# cat.victim(), cat.voice()
+# dog = Animal()
+# dog.victim(), dog.voice()
+# import lib
+# lib.manga
+# def h1(value):
+#     print(f'<h1>{value}</h1>')
+#
+#
+# def h2(value):
+#     print(f'<h2>{value}</h2>')
+#
+#
+# def h(number, value):
+#     return f'<h{number}>{value}</h{number}>'
+#
+#
+# h(5, "button doesn't work")
+# h(2, "button doesn't work")
+# h2("button doesn't work")
+file = open("test.txt", "r")
+text = file.read()
+print(text)
+file.close()
+f = open("test.txt", "a")
+f.write("world")
+f.write("!")
+f.close()
+f = open("test.txt", "w")
+f.write("1234")
